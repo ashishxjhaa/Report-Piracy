62b68a4f45eb54f1e203005d5b0991d084f5fc13
Ashish Jha <ashishxyzjha@gmail.com>
2025-08-11 10:37:32 +0530

    Added conatct detail

--- backend/src/creators.ts
+++ backend/src/creators.ts
@@ -1,4 +1,7 @@
+
 export const creators = [
-  { name: "Harkirat Singh", email: "creatorA@example.com" },
-  { name: "Apna College", email: "creatorB@example.com" }
+  { name: "Harkirat Singh", email: "harkirat.singh@gmail.com" },
+  { name: "Apna College", email: "shradhakhapra@gmail.com" },
+  { name: "Love Babbar", email: "lovebabbar3@gmail.com" },
+  { name: "Chai Aur Code", email: "hitesh@hiteshchoudhary.com"}
 ];
